d99d9c6d0fb7b099f4ae4f5d2918dbb4654f475e
DDoon <bean326@naver.com>
2023-06-12 22:33:12 +0900

    [NEXT-1] Setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      메인페이지 입니당
-    </main>
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">메인페이지 입니당 지라테스트</main>
   )
 }
